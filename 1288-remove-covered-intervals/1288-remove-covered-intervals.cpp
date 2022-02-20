0e262b4134648f4d0cff428ef7119ff2b26312a7
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2022-02-21 00:04:29 +0530

    Time: 33 ms (53.51%), Space: 11.2 MB (89.47%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(const pair<int,int>&a,const pair<int,int>&b){
+            if(a.first==b.first)
+                return a.second<b.second;
+            else
+                return a.first<b.first;
+        }
+    };
+    
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int count = 1;
+        pair<int,int> prev = make_pair(intervals[0][0],intervals[0][1]);
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals[i][1]>prev.second)
+            {
+                if(prev.first == intervals[i][0])
+                {
+                    prev.second  = intervals[i][1];
+                }
+                else
+                {
+                    prev.first = intervals[i][0];
+                    prev.second = intervals[i][1];
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
