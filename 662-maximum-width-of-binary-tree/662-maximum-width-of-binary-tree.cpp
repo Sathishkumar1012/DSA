667ebacad87136a30a4cfc3bc954d5585916801f
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2022-02-27 17:04:45 +0530

    Time: 4 ms (97.43%), Space: 17.3 MB (79.50%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -16,41 +16,31 @@ public:
             return 0;
         queue<pair<TreeNode*,int>>t;
         t.push({root,0});
-        long long result=1;
+        int result=1;
         while(!t.empty()) {
-            long long lmax=INT_MAX;
-            long long rmax=INT_MIN;
-            long long i=0;
+            //long long lmax=INT_MAX;
+            //long long rmax=INT_MIN;
+            int i=0;
             long long n=t.size();
             int start=t.front().second;
+            int end=t.back().second;
+            result=max(result,end-start+1);
             while(i<n) {
                 TreeNode* temp=t.front().first;
                 long long ind=t.front().second-start;
                 t.pop();
                 if(temp->left) {
                     t.push({temp->left,2*ind+1});
-                    lmax=min(lmax,2*ind+1);
-                    rmax=max(rmax,2*ind+1);
+                   // lmax=min(lmax,2*ind+1);
+                    //rmax=max(rmax,2*ind+1);
                 }
                 
                 if(temp->right) {
                     t.push({temp->right,2*ind+2});
-                    lmax=min(lmax,2*ind+2);
-                    rmax=max(rmax,2*ind+2);
+                   // lmax=min(lmax,2*ind+2);
+                    //rmax=max(rmax,2*ind+2);
                 }
                 i++;
-            }
-            
-            if(lmax==INT_MAX) {
-                lmax=0;
-                rmax=0;
-            }
-            
-            lmax=abs(lmax);
-            rmax=abs(rmax);
-            if(rmax-lmax+1 > result) {
-                result=rmax-lmax+1;
-                cout<<rmax<<"--"<<lmax<<endl;
             }
         }
         return result;
