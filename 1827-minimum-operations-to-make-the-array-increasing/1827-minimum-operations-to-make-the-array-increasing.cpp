2a0af83bc023bf1383eb0eb74a7060fa87e2e7df
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2022-04-05 15:37:46 +0530

    Time: 31 ms (15.72%), Space: 15.8 MB (74.78%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        if(nums.size()==1)
+            return 0;
+        if(nums.size()==2) {
+            if(nums[1]<=nums[0])
+                return nums[0]-nums[1]+1;
+            return 0;
+        }
+        
+        int count=0;
+        int i=1;
+        while(i<nums.size()) {
+            while(i<nums.size() && nums[i]>nums[i-1]) {
+                i++;
+            }
+            
+            if(i==nums.size())
+                return count;
+            count+=nums[i-1]-nums[i]+1;
+            nums[i]=nums[i-1]+1;
+            i++;
+        }
+        return count;
+    }
+};
