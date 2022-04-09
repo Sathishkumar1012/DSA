e39823a2f00f8e7fac9da1a4927bc9c2a880a41e
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2022-04-10 00:08:18 +0530

    Time: 26 ms (34.77%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     struct comp{
        bool operator()(pair<int,int>&a,pair<int,int>&b) {
-           return a.second>b.second;
+           return a.second<b.second;
        } 
     };
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int>res;
         unordered_map<int,int>mp;
         for(auto p:nums) {
             mp[p]++;
         }
         
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,comp>pq;
+        
+       /* if(mp.size()==1) {
+            res.push_back(nums[0]);
+            return res;
+        }*/
         
         for(auto it=mp.begin();it!=mp.end();it++) {
-            pq.push({it->second,it->first});
+            //cout<<it->first<<"->"<<it->second<<endl;
+            pq.push({it->first,it->second});
         }
         
         
-        vector<int>res;
         while(k--) {
-            res.push_back(pq.top().second);
+            res.push_back(pq.top().first);
+            //cout<<pq.top().first<<endl;
             pq.pop();
         }
         
