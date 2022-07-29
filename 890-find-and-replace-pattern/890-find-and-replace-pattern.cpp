f54fa8e9c235a0daa0c57873c5b9b503332a1868
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2022-07-30 00:51:48 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (92.07%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -4,7 +4,7 @@ public:
         
         vector<string>res;
         
-        for(auto p:words) {
+       /* for(auto p:words) {
             int flag=0;
             unordered_set<char>s;
             unordered_set<char>t;
@@ -36,7 +36,48 @@ public:
             
             if(flag==0)
                 res.push_back(p);
+        }*/
+        
+        unordered_map<char,int>m;
+        unsigned long long check=1;
+        int l=1;
+        m[pattern[0]]=1;
+        for(int i=1;i<pattern.length();i++) {
+            if(m[pattern[i]]!=0) {
+                check=(check*10)+m[pattern[i]];
+            }
+            
+            else {
+                l++;
+                check=check*10+l;
+                m[pattern[i]]=l;
+            }
         }
+        
+        for(auto p:words) {
+            m.clear();
+            unsigned long long check2=1;
+            l=1;
+            m[p[0]]=1;
+            for(int i=1;i<pattern.length();i++) {
+                if(m[p[i]]==0) {
+                    l+=1;
+                    check2=(check2*10)+l;
+                    m[p[i]]=l;
+                   
+                }
+            
+                else {
+                    check2=(check2*10)+m[p[i]];
+                }
+            }
+            
+            //cout<<check<<" "<<check2<<endl;
+            
+            if(check==check2)
+                res.push_back(p);
+        }
+        
         return res;
     }
 };
