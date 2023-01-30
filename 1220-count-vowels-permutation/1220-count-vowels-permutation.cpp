55528ac2d4e88591217c9da9dc524dc28c510cdf
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-01-30 22:09:09 +0530

    Time: 4 ms (85.11%), Space: 6 MB (83.62%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        long a,e,i,o,u;
-        a=e=i=o=u=1;
-        long mod=pow(10,9)+7;
-        
-        long a1,e1,i1,o1,u1;
-        
-        int j=2;
-        
-        while(j<=n) {
-            a1=(e+i+u)%mod;
-            e1=(a+i)%mod;
-            i1=(e+o)%mod;
-            o1=i%mod;
-            u1=(i+o)%mod;
+        if(n==1)
+            return 5;
+        int mod=1e9+7;
+        long long pa,pe,pi,po,pu;
+        pa=pe=pi=po=pu=1;
+        long long ca,ce,ci,co,cu;
+        int i=2;
+        int res=0;
+        while(i<=n) {
+            ca=pe+pu+pi;
+            ce=pa+pi;
+            ci=pe+po;
+            co=pi;
+            cu=pi+po;
             
-            a=a1;
-            e=e1;
-            i=i1;
-            o=o1;
-            u=u1;
-            j++;
+            ca=ca%mod;
+            ce=ce%mod;
+            ci=ci%mod;
+            co=co%mod;
+            cu=cu%mod;
+            //res=ca+ci+ce+co+cu;
+            pa=ca;
+            pe=ce;
+            pi=ci;
+            po=co;
+            pu=cu;
+           // cout<<pa<<" "<<pe<<" "<<pi<<" "<<po<<" "<<pu<<endl;
+            i++;
         }
-        return (a+e+i+o+u)%mod;
+        //cout<<res<<endl;
+        return (pa+pe+pi+po+pu)%mod;
     }
 };
