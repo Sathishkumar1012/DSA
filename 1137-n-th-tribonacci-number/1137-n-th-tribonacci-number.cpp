2f5b48887bc9ee73f59592bdce04f9771981ab74
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-01-30 19:35:35 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.73%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int check(int n,vector<int>&dp) {
-        if(dp[n]!=INT_MAX)
-            return dp[n];
-        
-        dp[n]=check(n-1,dp)+check(n-2,dp)+check(n-3,dp);
-        return dp[n];
-    }
     int tribonacci(int n) {
         if(n==0)
             return 0;
-        if(n==1)
+        if(n==1 || n==2)
             return 1;
-        if(n==2)
-            return 1;
-        
         vector<int>dp(n+1,INT_MAX);
         dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        dp[1]=dp[2]=1;
         
-        return check(n,dp);
+        for(int i=3;i<=n;i++) {
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
     }
 };
