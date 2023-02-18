aa09973978b4af79a29fb40f886e0ee316add19e
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-02-18 15:10:08 +0530

    Time: 265 ms (69.35%), Space: 71.2 MB (77.65%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -11,43 +11,19 @@
  */
 class Solution {
 public:
-    stack<int>s;
-    void check1(TreeNode* root,int level) {
-        if(!root)
+    void check(TreeNode* root1,TreeNode* root2,int level) {
+        if(!root1 || !root2)
             return;
-        check1(root->left,level+1);
-        check1(root->right,level+1);
-        
-        if(level%2==1) {
-            //cout<<"..."<<root->val<<endl;
-            s.push(root->val);
-        }
-       // return root;
-    }
-    
-    
-    void check2(TreeNode* root,int level) {
-        if(!root)
-            return;
-        
-        if(level%2==1) {
-            if(!s.empty())
-              //  cout<<"---"<<s.top()<<endl;
-            root->val=s.top();
-            s.pop();
-        }
-        
-        check2(root->left,level+1);
-        check2(root->right,level+1);
-        
-      //  return root;
+        if(level%2==1)
+            swap(root1->val,root2->val);
+        check(root1->left,root2->right,level+1);
+        check(root1->right,root2->left,level+1);
     }
     
     TreeNode* reverseOddLevels(TreeNode* root) {
-        stack<int>s;
-        check1(root,0);
-       // cout<<s.size()<<endl;
-        check2(root,0);
+        if(!root)
+            return root;
+        check(root->left,root->right,1);
         return root;
     }
 };
