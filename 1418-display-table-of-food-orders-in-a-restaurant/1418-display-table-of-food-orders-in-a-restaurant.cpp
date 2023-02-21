8c00232b9c592fc8d691387bb2a802cbcc1830af
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-02-21 23:20:33 +0530

    Time: 262 ms (47.74%), Space: 89.3 MB (44.59%) - LeetHub

--- 1418-display-table-of-food-orders-in-a-restaurant/1418-display-table-of-food-orders-in-a-restaurant.cpp
+++ 1418-display-table-of-food-orders-in-a-restaurant/1418-display-table-of-food-orders-in-a-restaurant.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
     vector<vector<string>> displayTable(vector<vector<string>>& orders) {
-        set<int>tables;
-        set<string>foods;
-        map<pair<int,string>,int>m;
-        
-        for(auto p:orders) {
-            tables.insert(stoi(p[1]));
-            foods.insert(p[2]);
-            pair<int,string>k;
-            k=make_pair(stoi(p[1]),p[2]);
-            
-            m[k]++;
-        }
-        
-        vector<vector<string>>result;
-        vector<string>t;
-        t.push_back("Table");
-        for(auto l:foods) {
-            t.push_back(l);
-        }
-        
-        result.push_back(t);
-        
-        
-        for(auto p:tables) {
-            vector<string>l;
-            l.push_back(to_string(p));
-            
-            for(auto k:foods) {
-                pair<int,string>s;
-                s=make_pair(p,k);
-                l.push_back(to_string(m[s]));
-            }
-            result.push_back(l);
-        }
-        
-        return result;
+    vector<unordered_map<string, int>> tables(501);
+    set<string> foods;
+    for (auto &v : orders) {
+        foods.insert(v[2]);
+        ++tables[stoi(v[1])][v[2]];
     }
+    vector<vector<string>> res;
+    for (auto t = 0; t <= 500; ++t) {
+        if (t > 0 && tables[t].empty())
+            continue;
+        res.push_back(vector<string>());
+        res.back().push_back(t == 0 ? "Table" : to_string(t));
+        for (auto it = begin(foods); it != end(foods); ++it) {
+            res.back().push_back(t == 0 ? *it : to_string(tables[t][*it]));
+        }
+    }
+    return res;
+}
 };
