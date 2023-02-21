043538bece85f95ed064c200217e4455df661129
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-02-21 22:30:43 +0530

    Time: 32 ms (43.92%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,25 +5,11 @@ public:
         int h=a.size()-1;
         
         while(l<h) {
-            int mid=l+(h-l)/2;
-            if(mid==0 || mid==a.size()-1)
-                return a[mid];
-            if(a[mid]!=a[mid-1] && a[mid]!=a[mid+1])
-                return a[mid];
-            
-            if(mid%2==1) {
-                if(a[mid-1]==a[mid])
-                    l=mid+1;
-                else
-                    h=mid-1;
-            }
-            
-            else {
-                if(a[mid]==a[mid+1])
-                    l=mid+1;
-                else
-                    h=mid-1;
-            }
+           int mid=(l+h)>>1;
+           if(a[mid]==a[mid^1])
+               l=mid+1;
+            else
+                h=mid;
            // cout<<a[mid]<<endl;
         }
         return a[l];
