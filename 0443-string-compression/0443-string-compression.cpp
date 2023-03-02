5236a6ccc76a90507f7a0418783b3fba060d63f0
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-03-03 00:10:25 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (42.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
     int compress(vector<char>& s) {
-        if(s.size()==1)
-            return 1;
-        vector<long>count(94,0);
-        vector<char>res;
-        
         int n=s.size();
-        
-        res.push_back(s[0]);
-        char b=s[0];
-        count[s[0]-'!']=1;
-        int i=1;
+        if(n==1)
+            return 1;
+        int i,j;
+        i=1;
+        j=1;
+        char t=s[0];
+        int count=1;
+        s[0]=s[0];
         
         while(i<n) {
-            while(i<n && s[i]==b) {
-                count[s[i]-'!']+=1;
+            while(i<n && s[i]==t) {
+                count++;
                 i++;
             }
             
-            for(int i=0;i<94;i++) {
-                if(count[i]>1) {
-                    string t=to_string(count[i]);
-                    //cout<<t<<endl;
-                    int j=0;
-                    while(j<t.length()) {
-                        res.push_back(t[j]);
-                        //cout<<t[j]<<endl;
-                        j++;
-                    }
+            
+            if(count>1) {
+                string t=to_string(count);
+                for(auto p:t) {
+                    s[j]=p;
+                    j++;
                 }
             }
             
-            count[b-'!']=0;
             
             if(i<n) {
-                res.push_back(s[i]);
-                b=s[i];
-                count[b-'!']=1;
+               s[j]=s[i];
+                j++;
+                count=1;
+                t=s[i];
             }
             
             i++;
         }
-       /* for(auto p:res){
-            cout<<p<<"--";
-        }
-        cout<<endl;*/
-        s=res;
-        return s.size();
+        return j;
     }
 };
