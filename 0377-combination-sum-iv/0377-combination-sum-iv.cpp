a78f7d67aa0b57bfa15742dad1d55368cb4eee4a
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-03-14 06:33:10 +0530

    Time: 4 ms (39.22%), Space: 6.6 MB (71.08%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         int n=nums.size();
+        sort(nums.begin(),nums.end());
         vector<unsigned int>dp(target+1,0);
         dp[0]=1;
         
@@ -9,6 +10,8 @@ public:
             for(auto p:nums) {
                 if(i>=p)
                     dp[i]+=dp[i-p];
+                else
+                    break;
             }
         }
         
