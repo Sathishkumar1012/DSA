694ecd1a9c57466e3c3f50aaf85b40366b0d783b
SathishKumar K <79698970+Sathishkumar1012@users.noreply.github.com>
2023-03-23 01:41:14 +0530

    Time: 510 ms (63.50%), Space: 139.3 MB (32.36%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    int result=INT_MAX;
+    
+    void helper(vector<bool>&vis,vector<vector<pair<int,int>>>&temp,int i) {
+        if(vis[i])
+            return;
+        vis[i]=true;
+        
+        for(auto &[v,dis]:temp[i]) {
+            result=min(result,dis);
+            helper(vis,temp,v);
+        }
+    }
+    
     int minScore(int n, vector<vector<int>>& roads) {
-        int ans = INT_MAX;
-        vector<vector<pair<int, int>>> gr(n+1);
-        for(auto edge : roads){ 
-            gr[edge[0]].push_back({edge[1], edge[2]}); // u-> {v, dis}
-            gr[edge[1]].push_back({edge[0], edge[2]}); // v-> {u, dis}
+        vector<vector<pair<int,int>>>temp(n+1);
+        for(auto p:roads) {
+            temp[p[0]].push_back({p[1],p[2]});
+            temp[p[1]].push_back({p[0],p[2]});
         }
-
-        vector<int> vis(n+1, 0);
-        queue<int> q;
-        q.push(1); vis[1] = 1;
-        while(!q.empty()){
-            auto node = q.front(); q.pop();
-            for(auto& [v, dis] : gr[node]){
-                ans = min(ans, dis);
-                if(vis[v]==0){
-                    vis[v] = 1;
-                    q.push(v);
-                }
-            }
-        }
-
-        return ans;
+        
+        vector<bool>vis(n+1,false);
+        
+        helper(vis,temp,1);
+        return result;
     }
 };
